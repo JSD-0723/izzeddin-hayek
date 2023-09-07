f0f71c1a46230934b2065c90cbc7118f82b6511e
Izzeddinhayek <izzeddinhayek@gmail.com>
2023-09-07 14:42:34 +0300

    week 5

---
+++ fetchdata.js
@@ -0,0 +1,66 @@
+const showLoader = () => {
+  const loader = document.getElementById("loader");
+  loader.style.display = "block";
+};
+
+const hideLoader = () => {
+  const loader = document.getElementById("loader");
+  loader.style.display = "none";
+};
+
+let data;
+
+async function fetchData() {
+  try {
+    const res = await fetch("https://tap-web-1.herokuapp.com/topics/list");
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const fetchedData = await res.json();
+    return fetchedData;
+  } catch (error) {
+    console.error("Error fetching data", error);
+    throw error;
+  }
+}
+
+function populateCards(data) {
+  const cardContainer = document.getElementById("cardContainer");
+  const cardTemplate = document.querySelector(".card");
+
+  data.map((dataItem) => {
+    const cardClone = cardTemplate.cloneNode(true);
+
+    cardClone.style.display = "flex";
+    cardClone.querySelector("img").src = "assets/" + dataItem.image;
+    cardClone.querySelector(".image-description").textContent =
+      dataItem.category;
+    cardClone.querySelector("h5").textContent = dataItem.topic;
+
+    const ratingStars = cardClone.querySelectorAll(".star");
+    for (let i = 0; i < dataItem.rating; i++) {
+      ratingStars[i].style.color = "gold";
+    }
+    cardClone.querySelector(".author").textContent = `Author: ${dataItem.name}`;
+
+    cardContainer.appendChild(cardClone);
+  });
+  console.log(data);
+  const topicsTotalNumber = document.getElementById("search-output");
+  topicsTotalNumber.innerHTML =
+    '"' + data.length.toString() + '" ' + topicsTotalNumber.innerHTML;
+}
+
+async function fetchDataAndPopulateCards() {
+  try {
+    const fetchedData = await fetchData();
+    if (fetchedData) {
+      populateCards(fetchedData);
+    }
+  } catch (error) {
+    console.error("Something went wrong. Web topics failed to load", error);
+  }
+}
+
+// Call the function to fetch data and populate cards
+fetchDataAndPopulateCards();
--- script.js
+++ script.js
@@ -1,8 +1,41 @@
-const darkModeButton = document.getElementById("darkModeButton");
+//Check for system's mode and if it has a previous preference in the Local Storage
+const checkForSystemDefault = () => {
+  if (localStorage.getItem("darkmode") == null) {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      document.documentElement.classList.add("darkmode");
+    }
+  }
+};
 
-darkModeButton.addEventListener("click", function () {
-  document.documentElement.classList.toggle("darkmode");
-});
+window.addEventListener("DOMContentLoaded", checkForSystemDefault);
+
+//Toggle Modes
+const toggleDarkMode = () => {
+  const rootElement = document.documentElement;
+
+  if (rootElement.classList.contains("darkmode")) {
+    rootElement.classList.remove("darkmode");
+    localStorage.setItem("darkmode", "light");
+  } else {
+    rootElement.classList.add("darkmode");
+    localStorage.setItem("darkmode", "dark");
+  }
+};
+
+//Check for Stored modes in the Local Storage
+const checkForDarkMode = () => {
+  const storedMode = localStorage.getItem("darkmode");
+  if (storedMode === "dark") {
+    document.documentElement.classList.add("darkmode");
+  }
+};
+window.addEventListener("DOMContentLoaded", checkForDarkMode);
+
+const darkModeButton = document.getElementById("darkModeButton");
+darkModeButton.addEventListener("click", toggleDarkMode);
 
 const favButton = document.getElementById("favButton");
 const favouritesMenu = document.querySelector(".favourites-menu");
